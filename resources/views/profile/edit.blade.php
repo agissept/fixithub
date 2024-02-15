03084aabc7bfada0e57129c7bf22f5ee4844c124
agis <agis.septiyanto@gmail.com>
2024-02-15 15:03:38 +0700

    hide shop info when customer

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,32 +1,34 @@
 <x-app-layout>
     <x-slot name="header">
-            {{ __('Pengaturan Akun') }}
+        {{ __('Pengaturan Akun') }}
     </x-slot>
 
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
+        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                @include('profile.partials.update-profile-information-form')
             </div>
+        </div>
 
+        @if(auth()->user()->role === \App\Http\Enum\UserRole::SERVICE_OWNER)
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-shop-information-form')
                 </div>
             </div>
+        @endif
 
-            <div class="p-4 sm:p-8 bg-white rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
+        <div class="p-4 sm:p-8 bg-white rounded-lg">
+            <div class="max-w-xl">
+                @include('profile.partials.update-password-form')
             </div>
         </div>
+
+        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                @include('profile.partials.delete-user-form')
+            </div>
+        </div>
+    </div>
 </x-app-layout>
