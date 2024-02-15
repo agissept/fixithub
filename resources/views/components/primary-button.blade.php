a53c723074f5feea3d949f3c1af5815573df9446
agis <agis.septiyanto@gmail.com>
2024-02-15 13:53:54 +0700

    update buttons

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,7 +1,7 @@
 <button {{ $attributes->merge(
          [
             'type' => 'submit',
-            'class' => 'inline-flex items-center bg-blue-primary text-white'
+            'class' => 'inline-flex items-center bg-blue-primary text-white px-4 py-2'
         ]
     ) }}>
     {{ $slot }}
