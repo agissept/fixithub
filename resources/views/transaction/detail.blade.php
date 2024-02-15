a6f72cfbd08d8f978413be67f7e9ed19f5d42876
agis <agis.septiyanto@gmail.com>
2024-02-15 14:36:59 +0700

    fix detail

--- resources/views/transaction/detail.blade.php
+++ resources/views/transaction/detail.blade.php
@@ -18,13 +18,16 @@
     @if($transaction->status === \App\Http\Enum\TransactionStatus::WAITING_CONFIRMATION->name)
         <div class="bg-white rounded-[11px] p-5 mt-5">
             <h2 class="color-blue-primary text-[21px] font-bold">Konfirmasi Pesanan</h2>
-            <form method="POST" class="flex justify-end mt-4" action="{{route('transaction.update.progress', [$transaction->id])}}">
+            <form method="POST" class="flex justify-end mt-4"
+                  action="{{route('transaction.update.progress', [$transaction->id])}}">
                 @csrf
-                <x-danger-button name='transaction_status' value="{{ \App\Http\Enum\TransactionStatus::REJECTED->name }}"
+                <x-danger-button name='transaction_status'
+                                 value="{{ \App\Http\Enum\TransactionStatus::REJECTED->name }}"
                                  class="mr-2">
                     Tolak
                 </x-danger-button>
-                <x-primary-button name='transaction_status' value="{{ \App\Http\Enum\TransactionStatus::CONFIRMED->name }}">Terima
+                <x-primary-button name='transaction_status'
+                                  value="{{ \App\Http\Enum\TransactionStatus::CONFIRMED->name }}">Terima
                 </x-primary-button>
             </form>
         </div>
@@ -33,18 +36,65 @@
             <h2 class="text-[21px] font-bold">Transaksi ini telah Anda batalkan</h2>
         </div>
     @else
-        <form class="bg-white rounded-[11px] p-5 mt-5 block" method="POST"
-              action="{{route('transaction.update.progress', [$transaction->id])}}" enctype="multipart/form-data">
+        <div class="bg-white rounded-[11px] p-5 mt-5">
+            <h2 class="color-blue-primary text-[21px] font-bold">Progress Pesanan</h2>
+            <div class="relative">
+{{--                <div class="absolute w-[4px] h-[100%] left-[8px] py-4">--}}
+{{--                    <div class="bg-blue-primary  h-[100%]"></div>--}}
+{{--                </div>--}}
+                @foreach($transaction->progress_histories as $history)
+                    <div class="flex mt-5">
+                        <div class="w-[20px] h-[20px] bg-white rounded-full z-10 flex items-center justify-center">
+                            <div class="w-[13px] h-[13px] bg-blue-primary rounded-full"></div>
+                        </div>
+                        <div class="ml-2">
+                            @switch($history->status)
+                                @case(\App\Http\Enum\TransactionStatus::PICK_UP_PROCESS->name)
+                                    <p>Proses pick up</p>
+                                    @break
+                                @case(\App\Http\Enum\TransactionStatus::WAITING_FOR_SERVICE->name)
+                                    <p>Dalam antrian servie</p>
+                                    @break
+                                @case(\App\Http\Enum\TransactionStatus::SERVICE_PROCESS->name)
+                                    <p>Proses service</p>
+                                    @break
+                                @case(\App\Http\Enum\TransactionStatus::WAITING_COURIER_FOR_SENDING_BACK_TO_CUSTOMER->name)
+                                    <p>Proses pengembalian ke customer</p>
+                                    @break
+                                @case(\App\Http\Enum\TransactionStatus::DONE->name)
+                                    <p>Selesai</p>
+                                    @break
+                                @default
+                                    <p>Status tidak ditemukan</p>
+                                    @break
+                            @endswitch
+                            @if($history->image)
+                                <img src="{{ route('images', $history->image) }}" class="w-3/4"/>
+                            @endif
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+        <form class=" bg-white rounded-[11px] p-5 mt-5 block" method="POST"
+              action="{{route('transaction.update.progress', [$transaction->id])}}"
+              enctype="multipart/form-data">
             @csrf
             <h2 class="color-blue-primary text-[21px] font-bold">Update Info Pemesanan</h2>
             <div class="mt-4">
                 <x-input-label for="transaction-status" :value="__('Proses')"/>
                 <select id="transaction-status" name="transaction_status"
                         class="mt-1 block w-full border-gray rounded-[4px]" required>
-                    <option value="{{ \App\Http\Enum\TransactionStatus::PICK_UP_PROCESS->name }}">Proses pick up</option>
-                    <option value="{{ \App\Http\Enum\TransactionStatus::WAITING_FOR_SERVICE->name }}">Dalam antrian servie
+                    <option value="{{ \App\Http\Enum\TransactionStatus::PICK_UP_PROCESS->name }}">Proses
+                        pick up
                     </option>
-                    <option value="{{ \App\Http\Enum\TransactionStatus::SERVICE_PROCESS->name }}">Proses service</option>
+                    <option value="{{ \App\Http\Enum\TransactionStatus::WAITING_FOR_SERVICE->name }}">
+                        Dalam antrian
+                        servie
+                    </option>
+                    <option value="{{ \App\Http\Enum\TransactionStatus::SERVICE_PROCESS->name }}">Proses
+                        service
+                    </option>
                     <option
                         value="{{ \App\Http\Enum\TransactionStatus::WAITING_COURIER_FOR_SENDING_BACK_TO_CUSTOMER->name }}">
                         Proses pengembalian ke customer
@@ -55,7 +105,8 @@
             </div>
             <div class="mt-4">
                 <x-input-label for="image" :value="__('Gambar(Opsional)')"/>
-                <x-text-input id="image" name="image" type="file" accept="image/*" class="mt-1 block w-full"/>
+                <x-text-input id="image" name="image" type="file" accept="image/*"
+                              class="mt-1 block w-full"/>
                 <x-input-error class="mt-2" :messages="$errors->get('image')"/>
             </div>
             <div class="mt-4">
