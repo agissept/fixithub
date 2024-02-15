3c59ca0050454213c8855f8679232e36a04e0fc2
agis <agis.septiyanto@gmail.com>
2024-02-15 08:17:48 +0700

    add shop information section in edit profile page

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,12 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
+            {{ __('Pengaturan Akun') }}
     </x-slot>
 
-    <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
@@ -14,6 +12,12 @@
             </div>
 
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('profile.partials.update-shop-information-form')
+                </div>
+            </div>
+
+            <div class="p-4 sm:p-8 bg-white rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
@@ -25,5 +29,4 @@
                 </div>
             </div>
         </div>
-    </div>
 </x-app-layout>
