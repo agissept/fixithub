5d46cf4d586f872e7fc333a572f1f5a4957bc8c3
agis <agis.septiyanto@gmail.com>
2024-02-16 17:40:44 +0700

    update profile with phone_number and address

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -14,34 +14,34 @@
         @method('patch')
 
         <div>
-            <x-input-label for="name" :value="__('Nama Lengkap')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
-            <x-input-error class="mt-2" :messages="$errors->get('name')" />
+            <x-input-label for="name" :value="__('Nama Lengkap')"/>
+            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)"
+                          required autofocus autocomplete="name"/>
+            <x-input-error class="mt-2" :messages="$errors->get('name')"/>
         </div>
 
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
-            <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-                <div>
-                    <p class="text-sm mt-2 text-gray-800">
-                        {{ __('Your email address is unverified.') }}
-
-                        <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            {{ __('Click here to re-send the verification email.') }}
-                        </button>
-                    </p>
-
-                    @if (session('status') === 'verification-link-sent')
-                        <p class="mt-2 font-medium text-sm text-green-600">
-                            {{ __('A new verification link has been sent to your email address.') }}
-                        </p>
-                    @endif
-                </div>
-            @endif
+            <x-input-label for="email" :value="__('Email')"/>
+            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full"
+                          :value="old('email', $user->email)" required autocomplete="username"/>
+            <x-input-error class="mt-2" :messages="$errors->get('email')"/>
         </div>
+
+        @if(auth()->user()->role === \App\Http\Enum\UserRole::CUSTOMER->value)
+            <div>
+                <x-input-label for="phone" :value="__('Nomor Telepon')"/>
+                <x-text-input id="phone" name="phone_number" type="text" class="mt-1 block w-full"
+                              :value="old('phone', $user->phone_number)" required autocomplete="phone"/>
+                <x-input-error class="mt-2" :messages="$errors->get('phone_number')"/>
+            </div>
+
+            <div>
+                <x-input-label for="address" :value="__('Alamat')"/>
+                <textarea id="address" name="address" type="text" class="mt-1 block w-full h-24 resize-none border-gray rounded-[4px]"
+                           required autocomplete="address">{{ old('address', $user->address) }}</textarea>
+                <x-input-error class="mt-2" :messages="$errors->get('address')"/>
+            </div>
+        @endif
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
