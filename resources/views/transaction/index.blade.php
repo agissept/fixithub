43f1242e0676be93a414844dc9630bbc93253acd
agis <agis.septiyanto@gmail.com>
2024-02-18 20:35:57 +0700

    fix badge

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -38,21 +38,8 @@
                     <p class="text-sm">{{ $transaction->created_at }}</p>
                 </div>
                 <div class="ml-auto">
-                    @if($transaction->status === \App\Http\Enum\TransactionStatus::WAITING_CONFIRMATION->name)
-                        <div class="text-[11px] text-center w-[70px] bg-blue-primary text-white rounded-[4px]">Pending
-                        </div>
-                    @elseif($transaction->status === \App\Http\Enum\TransactionStatus::CONFIRMED->name
-                            || $transaction->status === \App\Http\Enum\TransactionStatus::PICK_UP_PROCESS->name)
-                        <div class="text-[11px] text-center w-[70px] bg-amber-400 text-white rounded-[4px]">On Proses
-                        </div>
 
-                    @elseif($transaction->status === \App\Http\Enum\TransactionStatus::REJECTED->name)
-                        <div class="text-[11px] text-center w-[70px] bg-red-500 text-white rounded-[4px]">Batal
-                        </div>
-                    @else
-                        <div
-                                class="text-[11px] text-center w-[70px] bg-blue-primary text-white rounded-[4px]">{{$transaction->status ?? 'empty'}}</div>
-                    @endif
+                    @include('transaction.status-badge', ['transaction' => $transaction])
 
                     <a href="{{route('transaction.show', ['id' => $transaction->id])}}"
                        class="font-bold text-[12px] color-blue-primary mt-12 block">Lihat Detail</a>
